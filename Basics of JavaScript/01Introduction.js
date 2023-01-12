34bc731e16257237074734875cb06ad878be2b39
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-12 23:57:38 +0530

    Added comment about variables

--- Basics of JavaScript/01Introduction.js
+++ Basics of JavaScript/01Introduction.js
@@ -1,3 +1,5 @@
 // Consloe and log is the reserved kryword in JavaScript to print the text, string or number in terminal
 
 console.log("Hello World");
+
+// Let us learn about the variables in Javascript var, const and let
