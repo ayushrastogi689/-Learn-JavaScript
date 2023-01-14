b66d6306cf013289c59a9ecb7e98e1fdc84816de
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-14 23:57:14 +0530

    Javascript NaN - Not a Number Concept

--- Basics of JavaScript/03_ConversionOperation.js
+++ Basics of JavaScript/03_ConversionOperation.js
@@ -1,10 +1,23 @@
+// Case 1:
 let score1 = 33;
 
-console.log(typeof score1);
+console.log(typeof score1); // OP : number
+
+// Case 2: 
 
 let score2 = "33";
 // Here type of is treated as a method 
-console.log(typeof(score2));
+console.log(typeof(score2)); // OP: string
 
 // converting a variable value in a number
 let valueInNumber = Number(score2);
+
+console.log(typeof score2);  // OP: number
+
+// Case 3:
+
+let score3 = "33abc";
+
+let valueInNumber3 = Number(score3);
+
+console.log(typeof score2);  // OP: NaN - Not a number
