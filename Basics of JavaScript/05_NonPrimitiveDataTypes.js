a7a09458ea1f54bcbdfad17a8069c5e216927a0b
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-15 23:57:51 +0530

    Added Key values pairs in JS

--- Basics of JavaScript/05_NonPrimitiveDataTypes.js
+++ Basics of JavaScript/05_NonPrimitiveDataTypes.js
@@ -4,3 +4,12 @@ let a;
 console.log(a); // The op will be undefined by itself
 
 // Objects in JS are treated as non primitive data types
+const items = {
+  "Ayush" : true;
+  "Rishabh" : false ;
+  "Mahima" : 25;
+  "Caryen" : undefined;
+  "Pranav" : null;
+}
+
+console.log(["Ayush"]);
