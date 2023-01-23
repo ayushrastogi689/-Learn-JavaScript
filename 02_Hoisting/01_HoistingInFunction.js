4b9a8a37480253d55e79fba8f0c45fa750482dc0
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-23 18:24:34 +0000

    Added comment

--- 02_Hoisting/01_HoistingInFunction.js
+++ 02_Hoisting/01_HoistingInFunction.js
@@ -6,4 +6,5 @@ function getName(){
     console.log("I Am Ayush");
 }
 
-// Here we are trying to access the function get name even before we have initialized it
+// Here we are trying to access the function get name even before we have initialized it.
+// the function gets hoisted in JavaScript and we the output: I am Ayush      insted of getting any error
