a61d73701cc6096be0389b4770dfa9f4e0bafd27
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-23 19:48:39 +0000

    Shortest JS program

---
+++ 03_Function-invocation/02_ShortestJSprogram.js
@@ -0,0 +1,13 @@
+// The empty file is called as Shortest JS program.
+
+// even though there is no code to execute... 
+// still JS engine is doing a lot of things behind the scenes 
+
+// 1. AS soon as we run this program in google chrome and do a inspect.
+// 2. The JS engine creates a global execution context and also sets up the memory space
+// 3. JS engine also does something interesting, it creates something called window (a global object) in global space.
+// 4. this points to the global object i.e. window object
+
+// Q What is global space? 
+// Ans Whenever we write any JavaScript code which not inside any block or funtion then it 
+// is called as golbal space. 
