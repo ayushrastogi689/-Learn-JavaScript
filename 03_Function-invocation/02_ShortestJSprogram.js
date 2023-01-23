09f1e951706e4c57d280447d8e9420cce8ecb45d
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-23 19:55:41 +0000

    Added about global object which is window

--- 03_Function-invocation/02_ShortestJSprogram.js
+++ 03_Function-invocation/02_ShortestJSprogram.js
@@ -6,8 +6,13 @@
 // 1. AS soon as we run this program in google chrome and do a inspect.
 // 2. The JS engine creates a global execution context and also sets up the memory space
 // 3. JS engine also does something interesting, it creates something called window (a global object) in global space.
-// 4. this points to the global object i.e. window object
+// 4. "this" points to the global object i.e. window object
 
 // Q What is global space? 
-// Ans Whenever we write any JavaScript code which not inside any block or funtion then it 
+// Ans Whenever we write any JavaScript code which not inside any block or function then it 
 // is called as golbal space. 
+
+// Whenever we create any variable or function inside the global space it gets attached to 
+// the window     
+// For Ex_1 var a = 10;  -> console.log(window.a); // OP : 10
+// Ex_2 var a = 10;  -> console.log(this.a); // OP : 10
