2fd41de7009ee3d4462ae65139b0002a7d570f83
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-23 18:19:47 +0000

    Added 02_Hoisting in JS

---
+++ 02_Hoisting/01_HoistingInFunction.js
@@ -0,0 +1,7 @@
+// Function invocation and Variable environment
+
+getName();
+
+function getName(){
+    console.log("I Am Ayush");
+}
--- Basics of JavaScript/01Introduction.js
+++
@@ -1,3 +0,0 @@
-// Consloe and log is the reserved kryword in JavaScript to print the text, string or number in terminal
-
-console.log("Hello World");
