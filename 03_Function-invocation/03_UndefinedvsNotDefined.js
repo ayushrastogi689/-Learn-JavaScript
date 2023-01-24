0c4fdf711c8489c8e80a3ba666ead0bed427c668
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-24 18:19:03 +0000

    Added what is Undefined

---
+++ 03_Function-invocation/03_UndefinedvsNotDefined.js
@@ -0,0 +1,7 @@
+// Undefined vs Not Defined
+
+console.log(a); 
+// Before initialization of a, a is only declared. JS engine has allocated the memory to a: undefined
+var a = 7;
+
+// Once the initialization has occured the values of a gets updated to 7.
