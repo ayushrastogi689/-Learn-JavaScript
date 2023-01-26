790cd498bd5c3f19953028f66c67375235ff4ead
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-26 21:59:10 +0000

    Added about errors

---
+++ 05_let & const | Temporal dead zone/02_TypesOfError.js
@@ -0,0 +1,7 @@
+// 1. Reference Error ; (i) x is not defined
+                        // (ii) Cannot access "a" before initialization
+
+
+// 2. Syntax Error : (i) Missing initializer in a const declaration
+
+// 3. TypeError : (i) Assingment to a constant variable
