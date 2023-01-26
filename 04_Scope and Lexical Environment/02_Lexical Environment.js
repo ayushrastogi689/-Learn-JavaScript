4737067e217e9dcd82cf5ca767ca511dad9e7040
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-26 20:22:19 +0000

    Added about Lexical environment

--- 04_Scope and Lexical Environment/02_Lexical Environment.js
+++ 04_Scope and Lexical Environment/02_Lexical Environment.js
@@ -1,2 +1,7 @@
-// Scope is directly related to lexical environment
+// Scope is directly related to Lexical Environment
 
+// 1. Whenever a Execution context is created a lexical enviornment is also created 
+
+// Lexical means a heirarchy or a sequence 
+
+// 2. Lexical environment is the local memory along with the lexical environment of its parent.
