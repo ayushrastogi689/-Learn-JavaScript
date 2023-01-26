39609d6eaceeebc14e9909931c05fc60a1d0c242
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-26 21:42:14 +0000

    Added hoistiong of let and const

---
+++ 05_let & const | Temporal dead zone/01_let_&_const_hoisted.js
@@ -0,0 +1,14 @@
+// let and const declarations are also hoisted in JavaScript
+
+// 1.
+console.log(a);
+var a = 10;
+// var a is presend in global space holding a special type of placeholder called "undefined"
+
+
+
+// 2.
+console.log(b);
+let b = 20;
+
+// let is hoisted in Temporal dead zone inside Script
