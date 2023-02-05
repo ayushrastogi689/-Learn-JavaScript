9c092d95e88bed5e02607dd4fa9024097ce2af17
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-05 23:35:31 +0530

    Added the definition of set Timeout.

--- Closures in JS/setTimeOut.js
+++ Closures in JS/setTimeOut.js
@@ -7,3 +7,5 @@ function x() {
 console.log("JavaScript");
 
 x();
+
+// 1 Set time out stores the function in a different place and attaches a timer to it. When the timer is finished, it rejoins the call stack and executed. 
