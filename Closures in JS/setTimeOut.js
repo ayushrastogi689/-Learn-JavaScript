efe1458e3c5be33b90b4f27644fbd74db60c4064
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-06 00:00:41 +0530

    Added Case 2 of the question

--- Closures in JS/setTimeOut.js
+++ Closures in JS/setTimeOut.js
@@ -13,5 +13,15 @@ console.log("JavaScript");
 x();
 
 // Case 2: Variable declaration with "let"
+function y() {
+  for (var i = 0; i<= 5; i++)
+  {
+    setTimeout(function () {
+      console.log(i); 
+    }, i + 10;
+  }
+console.log(" Hello Js");
 
+// A joke: 
+// Time, tide and JavaScript waits for none.. Haha
 
