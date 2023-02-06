8fcc717f23a73e24a32d74851421437304a365f4
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-06 23:53:03 +0530

    Added more uses of closures

--- Closures in JS/UsesOfClosures.js
+++ Closures in JS/UsesOfClosures.js
@@ -1,4 +1,9 @@
-// // The uses of closures: 
-// 1. Module design pattern 
-// 2. Currying
-// 3. Functions like once
+// // // The uses of closures: 
+// // 1. Module design pattern 
+// // 2. Currying
+// // 3. Functions like once
+// 4. Memoize
+// 5. Maintain state in Async function 
+// 6. setTimeouts
+// 7. Iterators
+
