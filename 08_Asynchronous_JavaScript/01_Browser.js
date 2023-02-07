b5eb0317ba16a8e881cac2009f9d6e8a9a8a11e4
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-07 23:47:10 +0530

    Added about Web APIs

--- 08_Asynchronous_JavaScript/01_Browser.js
+++ 08_Asynchronous_JavaScript/01_Browser.js
@@ -5,3 +5,14 @@
 // 2. Browser has a timer inside it
 // 3. Browser has url bar through which we can write the domain name and access the website
 // 4. Geo location access
+
+// The JS enine needs something to access the super powers of Browsers called as Web APIs
+
+// Web APIs :
+ 
+// console
+// setTimeout
+// Document.getElemet = DOM APIs
+// fetch()
+// Local Storage
+// Location
