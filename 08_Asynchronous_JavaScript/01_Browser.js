f27cc8188f69605acc6dbca2538eae97999063c7
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-07 23:42:41 +0530

    Added about browser and its power

---
+++ 08_Asynchronous_JavaScript/01_Browser.js
@@ -0,0 +1,7 @@
+// Browser is one of the most remarkable creation in the history of mankind
+
+// Browser consist of JS engine
+// 1. Browser has local storage
+// 2. Browser has a timer inside it
+// 3. Browser has url bar through which we can write the domain name and access the website
+// 4. Geo location access
