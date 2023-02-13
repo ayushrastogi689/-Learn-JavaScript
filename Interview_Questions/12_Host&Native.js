f14263a018fccfaefbe1e97a91c2085fda3d4c57
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-13 23:42:43 +0530

    Added about host and native objects

---
+++ Interview_Questions/12_Host&Native.js
@@ -0,0 +1,5 @@
+// Q23:   What's the difference between Host objects and Native objects?  
+
+// Answer
+// Native objects are objects that are part of the JavaScript language defined by the ECMAScript specification, such as String, Math, RegExp, Object, Function, etc.
+// Host objects are provided by the runtime environment (browser or Node), such as window, XMLHTTPRequest, etc.
