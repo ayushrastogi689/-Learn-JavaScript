ab46fd023052214a932edf2585736571dd5b458b
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-14 23:47:09 +0530

    Added about classes in Javascript

---
+++ Interview_Questions/15_Classes.js
@@ -0,0 +1,11 @@
+// Introduced in the ES6 version, classes are nothing but syntactic sugars for constructor functions.
+// They provide a new way of declaring constructor functions in javascript.  Below are the examples of how classes are declared and used:
+
+// Before ES6 version, using constructor functions
+
+function Student(name,rollNumber,grade,section){
+  this.name = name;
+  this.rollNumber = rollNumber;
+  this.grade = grade;
+  this.section = section;
+}A
