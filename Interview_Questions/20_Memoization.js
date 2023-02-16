0ef94a3d2e19df536459be197eab18329dc49b18
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-16 23:59:52 +0530

    Added

--- Interview_Questions/20_Memoization.js
+++ Interview_Questions/20_Memoization.js
@@ -1,5 +1,5 @@
-//What is memoization
-//Memoization is a programming technique which attempts to increase a function’s performance by caching its previously computed results. Each time a memoized function is called, its parameters are used to index the cache. If the data is present, then it can be returned, without executing the entire function. Otherwise the function is executed and then the result is added to the cache. Let's take an example of adding function with memoization,
+//Q What is memoization
+//Ans Memoization is a programming technique which attempts to increase a function’s performance by caching its previously computed results. Each time a memoized function is called, its parameters are used to index the cache. If the data is present, then it can be returned, without executing the entire function. Otherwise the function is executed and then the result is added to the cache. Let's take an example of adding function with memoization,
 
 const memoizAddition = () => {
   let cache = {};
