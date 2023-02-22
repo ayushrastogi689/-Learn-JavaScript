709b671fcba9719159522f103f6e15c88a026617
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-22 23:18:25 +0530

    Added about cookies

---
+++ Interview_Questions/25_Cookie.js
@@ -0,0 +1,5 @@
+// Q: What is a Cookie
+// Ans: A cookie is a piece of data that is stored on your computer to be accessed by your browser. Cookies are saved as key/value pairs. 
+// For example, you can create a cookie named username as below,
+
+document.cookie = "username=John";
