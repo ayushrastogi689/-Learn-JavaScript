e159b436d5ce72fea61eca37f1edf02cd415eb1e
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-02-24 00:02:37 +0530

    Added about the differences between Cookie, Local Storage & Session Storage

--- Interview_Questions/Differences.js
+++ Interview_Questions/Differences.js
@@ -1,8 +1,8 @@
-// What are the differences between cookie, local storage and session storage
-// Below are some of the differences between cookie, local storage and session storage,
+//Ques:  What are the differences between cookie, local storage and session storage
+// Ans:  Below are some of the differences between cookie, local storage and session storage,
 
-Feature     	                                          Cookie	                           Local storage	                            Session storage
-Accessed on client or server side	               Both server-side & client-side	          Client-side only	                        Client-side only
-Lifetime	                                         As configured                          Using Expires option until deleted	      Until tab is closed
-SSL                                                support	Supported	                    Not supported	                            Not supported
-Maximum data size	                               4KB	                                    5 MB	                                    5MB
+// Feature     	                                          Cookie	                             Local storage	                            Session storage
+// Accessed on client or server side	               Both server-side & client-side	         Client-side only	                          Client-side only
+// Lifetime	                                         As configured                           Using Expires option until deleted	        Until tab is closed
+// SSL support	                                     Supported	                             Not supported	                            Not supported
+// Maximum data size	                               4KB	                                   5 MB	                                      5MB
